4fc8a627a1fab7c4984f75ce525e5634d831a1c1
Eden Mesfun <emesfun@gmail.com>
2017-12-24 01:21:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,16 +2,16 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, x)
-  
+    elsif board[x] == "X"
+    return false
+  else board[x] == "O"
+    return false
   
   
   if x == 0
     return true
   elsif x == 0 || x == 1 || x == 2 || 3
     return true
-  elsif board[x] == "X"
-    return false
-  else board[x] == "O"
-    return false
+
   end 
 end
